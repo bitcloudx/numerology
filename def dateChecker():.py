cdcc50995bbaaed1f26f4040c27f939f92b8952f
{ Bitcloud } = True <49758269+bitcloudx@users.noreply.github.com>
2022-03-13 22:31:21 -0400

    just changed and updated the list so it display's better

--- def dateChecker():.py
+++ def dateChecker():.py
@@ -5,7 +5,7 @@ def dateChecker():
     year2 = 0
     i = 0
     list_of_dates = []
-    while i < 48180:
+    while i < 50000:
         if year2 <= 9:
          year2_format = '0'+str(year2)
         else:
@@ -16,7 +16,7 @@ def dateChecker():
         print('\n')
         leap_years = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96]
         sum_of_dates = days + months + year1 + year2
-        dates = (str(days)+'/'+str(months)+'/'+str(year1)+year2_format)
+        dates = (str(months)+'/'+str(days)+'/'+str(year1)+year2_format)
         if sum_of_dates != 68:
             if days >= 31 and months in [1, 3, 5, 7, 8, 10, 12] or days >= 30 and months in [4, 6, 9, 11] or days >= 28 and months == 2 and year2 not in leap_years or days >= 29 and months == 2 and year2 in leap_years:
                 print('month changer here')
